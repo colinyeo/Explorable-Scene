21b3adc2f559e48c5e4d70a8b7d6c1764d424a48
Colin Yeo <colinyeo@alum.calarts.edu>
2019-02-27 23:54:39 -0800

    Added Assets

---
+++ Assets/TalkToHorse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalkToHorse : MonoBehaviour {
+
+	public GameObject hero;
+	public Text horseSpeech;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(hero.transform.position.y > 24){
+			horseSpeech.text = "I've been expecting you. I am a horse and I am glad to see you. I am so proud.";
+		}
+	}
+}
---
+++ Assets/UpHorse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpHorse : MonoBehaviour {
+
+	public GameObject hero;
+	public float horseHeight;
+
+	// Use this for initialization
+	void Start () {
+		horseHeight = transform.position.y;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hero.transform.position.y > 24) {
+			moving ();
+		}
+	}
+
+	public void moving (){
+		if (transform.position.y < 0.5) {
+			horseHeight = horseHeight + 0.1f;
+			transform.position = new Vector3 (transform.position.x, horseHeight, transform.position.z);
+		}
+	}
+}
